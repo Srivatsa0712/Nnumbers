daa05124519ffc72973359973f8b244f4475fab5
Srivatsa Sugurkar <79782950+Srivatsa0712@users.noreply.github.com>
2021-11-20 10:56:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function natsum()
+{
+var a = document.form.N.value;
+var i;
+var sum = 0;
+
+var N= new Number(a);
+for(i=1;i<=N;i++)
+sum=sum+i;
+
+document.write("<h2>sum of 1 to "+a+" natural numbers is "+sum+" </h2>");
+}
